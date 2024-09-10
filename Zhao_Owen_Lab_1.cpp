4b5b6340ec83a0e9953c7be119d99083fc3a70b7
OwenZhaoStudent <owenz19@nycstudents.net>
2024-09-10 16:58:25 -0400

    Lab 1

---
+++ Zhao_Owen_Lab_1.cpp
@@ -0,0 +1,40 @@
+//Owen Zhao
+//Lab 1
+//Sep 09, 2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+/*The code here makes each line for the code and adds a new line with endl. 
+As a result, I made the sides of the box and all the text letter by letter and made new lines to make it less claustrohpbic*/
+
+    cout << "###################################################################################" << endl;
+    cout << "#                                                                                 #" << endl;
+    cout << "#  Ways to access the Task Manager on your Windows computer:                      #" << endl;
+    cout << "#                                                                                 #" << endl;
+    cout << "#      Press the key combination Ctrl + Shift + Escape                            #" << endl;
+    cout << "#                                                                                 #" << endl;
+    cout << "#      Press the key combination Ctrl + Alt + Delete and select “Task Manager”    #" << endl;
+    cout << "#                                                                                 #" << endl;
+    cout << "#      Type “Task Manager” in the Windows Start menu search                       #" << endl;
+    cout << "#                                                                                 #" << endl;
+    cout << "###################################################################################" << endl;
+
+    return 0;
+    
+/*###################################################################################
+#                                                                                 #
+#  Ways to access the Task Manager on your Windows computer:                      #
+#                                                                                 #
+#      Press the key combination Ctrl + Shift + Escape                            #
+#                                                                                 #
+#      Press the key combination Ctrl + Alt + Delete and select “Task Manager”    #
+#                                                                                 #
+#      Type “Task Manager” in the Windows Start menu search                       #
+#                                                                                 #
+###################################################################################*/
+
+}
